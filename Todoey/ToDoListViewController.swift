486b3e52574f1a0dbc560c5555ec7f95f68d27b9
otma88 <otma88@gmail.com>
2018-09-14 05:37:08 +0200

    Unos podataka koristeći UserDefaults bazu podataka

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -13,8 +13,14 @@ class ToDoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         
     }
     
@@ -58,6 +64,8 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
             
